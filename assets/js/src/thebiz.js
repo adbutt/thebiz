cd94d879d7fe9d6c84c5950a88366095b4dcff0f
Adam butterworth <adbutt@gmail.com>
2017-02-08 16:48:33 +0800

    first commit - naked 10UP

---
+++ assets/js/src/thebiz.js
@@ -0,0 +1,12 @@
+/**
+ * thebiz
+ * http://workpowermedia.com.au
+ *
+ * Copyright (c) 2017 Adam butterworth
+ * Licensed under the GPL-2.0+ license.
+ */
+
+// ( function( window, undefined ) {
+// 	'use strict';
+
+// } )( this );
---
+++ tasks/options/sass.js
@@ -0,0 +1,11 @@
+module.exports = {
+	all: {
+		options: {
+			precision: 2,
+			sourceMap: true
+		},
+		files: {
+			'assets/css/thebiz.css': 'assets/css/sass/thebiz.scss'
+		}
+	}
+};
